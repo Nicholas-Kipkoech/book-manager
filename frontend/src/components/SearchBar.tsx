e0783c4ce4d3b1593a8d098809ac29f61b5f8659
Nicholas Kipkoech <nickey968@gmail.com>
2024-06-08 12:33:43 +0300

    add input filed

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const SearchBar = () => {
   return (
     <div className="input-wrapper">
+      <FaSearch id="search-icon" />
       <input placeholder="search for a book...." />
     </div>
   );
