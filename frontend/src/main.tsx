42f5beb689b8f8677812f895ae135a655ed42e59
Nicholas Kipkoech <nickey968@gmail.com>
2024-06-08 08:06:00 +0300

    feat: add graphql api client

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+/**
+ * Connecting to graphql API from the local server
+ */
+
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:3000",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
